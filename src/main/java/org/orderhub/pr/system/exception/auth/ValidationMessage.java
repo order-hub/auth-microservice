b12da895d2a21925d67e0aaeef8e8d350a859109
qhtneo <hj9671@gmail.com>
2025-02-21 06:28:14 +0900

    Refactor: 이름변경(ExceptionMessage -> ValidationMessage), 접근제어자(PROTECT -> PRIVATE) 변경

--- src/main/java/org/orderhub/pr/system/exception/auth/ValidationMessage.java
+++ src/main/java/org/orderhub/pr/system/exception/auth/ValidationMessage.java
@@ -3,8 +3,8 @@ package org.orderhub.pr.system.exception.auth;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ExceptionMessage {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ValidationMessage {
     public static final String USERNAME_MESSAGE = "아이디는 알파벳 소문자와 숫자를 사용하며, 숫자로 시작할 수 없습니다. (3~30자리)";
     public static final String PASSWORD_MESSAGE = "비밀번호는 영문, 숫자, 특수문자를 모두 사용하여 8자리 이상 100자리 이하입니다.";
     public static final String PHONE_NUMBER_MESSAGE = "올바른 전화번호 형식이 아닙니다.";
