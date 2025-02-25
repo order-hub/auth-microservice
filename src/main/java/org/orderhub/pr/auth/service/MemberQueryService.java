3f64875aab72269cf2c9d0663800f08aa3cb76bf
qhtneo <hj9671@gmail.com>
2025-02-25 21:29:44 +0900

    feat: 회원 조회 서비스 작성     - 전체 회원 조회     - 단일 회원 조회         - 아이디         - 이름         - 상태별         - 직책별     - 회원 존재 여부 확인     - 이미 사용중인 username 확인     - 이미 사용중인 전화번호 확인

--- src/main/java/org/orderhub/pr/auth/service/MemberQueryService.java
+++ src/main/java/org/orderhub/pr/auth/service/MemberQueryService.java
@@ -1,16 +1,27 @@
 package org.orderhub.pr.auth.service;
 
+import org.orderhub.pr.auth.domain.Member;
+import org.orderhub.pr.auth.domain.MemberRole;
+import org.orderhub.pr.auth.domain.MemberStatus;
 import org.orderhub.pr.auth.dto.MemberQueryDto.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MemberQueryService {
 
-    // TODO: 추후에 해당 메서드 사용 예정
-    FindMemberResponse getAllMembers();
-    // TODO: 추후에 해당 메서드 사용 예정
-    FindMemberByIdResponse getMemberById(UUID id);
-    // TODO: 추후에 해당 메서드 사용 예정
-    FindMemberByUsernameResponse findByUsername(String username);
+    List<FindMemberByIdResponse> findAllMembers();
+
+    Member findMemberEntityById(UUID id);
+    Optional<FindMemberByIdResponse> findMemberById(UUID id);
+    Optional<FindMemberByUsernameResponse> findByUsername(String username);
+
+    List<FindMemberByIdResponse> findByStatus(MemberStatus status);
+    List<FindMemberByIdResponse> findByRole(MemberRole role);
+
+    boolean existsByUsername(String username);
+    boolean existsByTel(String tel);
+    boolean existsById(UUID id);
 
 }
