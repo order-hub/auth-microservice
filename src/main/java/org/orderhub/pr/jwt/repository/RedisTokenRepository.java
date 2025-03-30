0db6fb764db108e9594ae712e0c93cebea4b50bf
qhtneo <hj9671@gmail.com>
2025-03-31 03:28:31 +0900

    chore: RedisTokenRepository 사용하지않는 주석 제거

--- src/main/java/org/orderhub/pr/jwt/repository/RedisTokenRepository.java
+++ src/main/java/org/orderhub/pr/jwt/repository/RedisTokenRepository.java
@@ -17,26 +17,15 @@ public class RedisTokenRepository implements TokenRepository {
         redisTemplate.opsForValue().set(memberId.toString() + ":refresh", refreshToken);
     }
 
-//    @Override
-//    public void saveAccessToken(UUID memberId, String accessToken) {
-//        redisTemplate.opsForValue().set(memberId.toString()  + ":access", accessToken);
-//    }
-
     @Override
     public Optional<String> getRefreshToken(UUID memberId) {
         String refreshToken = redisTemplate.opsForValue().get(memberId.toString()  + ":refresh");
         return Optional.ofNullable(refreshToken);
     }
 
-//    @Override
-//    public Optional<String> getAccessToken(UUID memberId) {
-//        String accessToken = redisTemplate.opsForValue().get(memberId.toString()  + ":access");
-//        return Optional.ofNullable(accessToken);
-//    }
-
     @Override
     public void deleteTokens(UUID memberId) {
         redisTemplate.delete(memberId.toString()  + ":access");
-        redisTemplate.delete(memberId.toString()  + ":refresh");
+        redisTemplate.delete(memberId + ":refresh");
     }
 }
